5c6e06b81ddbc9bf55c7ff920afcf79440170b8e
tomonori.suzuki <tomonori.suzuki@bizreach.co.jp>
2022-07-23 14:25:38 +0900

    必要ないtenantを取得しない

--- go/isuports.go
+++ go/isuports.go
@@ -668,14 +668,14 @@ func tenantsBillingHandler(c echo.Context) error {
 	//   を合計したものを
 	// テナントの課金とする
 	ts := []TenantRow{}
-	if err := adminDB.SelectContext(ctx, &ts, "SELECT * FROM tenant ORDER BY id DESC"); err != nil {
+	if err := adminDB.SelectContext(ctx, &ts, "SELECT * FROM tenant WHERE id <= ? ORDER BY id DESC LIMIT 10", beforeID); err != nil {
 		return fmt.Errorf("error Select tenant: %w", err)
 	}
 	tenantBillings := make([]TenantWithBilling, 0, len(ts))
 	for _, t := range ts {
-		if beforeID != 0 && beforeID <= t.ID {
-			continue
-		}
+		//if beforeID != 0 && beforeID <= t.ID {
+		//	continue
+		//}
 		err := func(t TenantRow) error {
 			tb := TenantWithBilling{
 				ID:          strconv.FormatInt(t.ID, 10),
