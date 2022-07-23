990a019d865db0da58124e6e8dd40c90ec761410
Takahiro Ishitsuka <taka.rock.i4@gmail.com>
2022-07-23 15:02:21 +0900

    検証

--- go/isuports.go
+++ go/isuports.go
@@ -1350,19 +1350,19 @@ func competitionRankingHandler(c echo.Context) error {
 		return fmt.Errorf("error Select tenant: id=%d, %w", v.tenantID, err)
 	}
 
-	go func() {
-		// 書き込み結果は不要なのでgorotineで実行する
-		if _, err := adminDB.ExecContext(
-			ctx,
-			"INSERT INTO visit_history (player_id, tenant_id, competition_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?)",
-			v.playerID, tenant.ID, competitionID, now, now,
-		); err != nil {
-			fmt.Errorf(
-				"error Insert visit_history: playerID=%s, tenantID=%d, competitionID=%s, createdAt=%d, updatedAt=%d, %w",
-				v.playerID, tenant.ID, competitionID, now, now, err,
-			)
-		}
-	}()
+	//go func() {
+	// 書き込み結果は不要なのでgorotineで実行する
+	if _, err := adminDB.ExecContext(
+		ctx,
+		"INSERT INTO visit_history (player_id, tenant_id, competition_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?)",
+		v.playerID, tenant.ID, competitionID, now, now,
+	); err != nil {
+		fmt.Errorf(
+			"error Insert visit_history: playerID=%s, tenantID=%d, competitionID=%s, createdAt=%d, updatedAt=%d, %w",
+			v.playerID, tenant.ID, competitionID, now, now, err,
+		)
+	}
+	//}()
 
 	var rankAfter int64
 	rankAfterStr := c.QueryParam("rank_after")
