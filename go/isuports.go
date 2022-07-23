48232f07b512ac4ef394db4ed94446183fae1162
Shutarou Takayama <hi.mountain110916@gmail.com>
2022-07-23 17:53:02 +0900

    fix

--- go/isuports.go
+++ go/isuports.go
@@ -1250,8 +1250,9 @@ func playerHandler(c echo.Context) error {
 		ctx,
 		&ps,
 		// 最後にCSVに登場したスコアを採用する = row_numが一番大きいもの
-		"SELECT ps.ID AS ctitle, ps.score AS pscore FROM player_score AS ps JOIN (SELECT competition_id, MAX(row_num) AS newest FROM player_score WHERE player_id = ? GROUP BY competition_id) AS ps2 ON ps.competition_id = ps2.competition_id AND ps.row_num = newest LEFT JOIN competition AS c ON c.id = ps.competition_id  WHERE ps.tenant_id =? AND ps.player_id = ? ",
+		"SELECT ps.ID AS ctitle, ps.score AS pscore FROM player_score AS ps JOIN (SELECT competition_id, MAX(row_num) AS newest FROM player_score WHERE player_id = ? AND tenant_id = ? GROUP BY competition_id) AS ps2 ON ps.competition_id = ps2.competition_id AND ps.row_num = newest LEFT JOIN competition AS c ON c.id = ps.competition_id  WHERE ps.tenant_id =? AND ps.player_id = ? ",
 		p.ID,
+		v.tenantID,
 		v.tenantID,
 		p.ID,
 	); err != nil {
