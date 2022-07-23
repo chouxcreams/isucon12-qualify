42705c232a621dc674d841c240077269face52a0
Takahiro Ishitsuka <taka.rock.i4@gmail.com>
2022-07-23 16:29:10 +0900

    2は実行しない

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@
 # デーモン名を確認、指定する
 
 echo "start deploy ${USER}"
-for host_name in isucon12-1 isucon12-2 isucon12-3;do
+for host_name in isucon12-1 isucon12-3;do
     echo $host_name
     ssh $host_name "cd webapp && make apply" &
 done
