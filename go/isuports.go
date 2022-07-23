8774f5da3afd6fe4b96693cebf20ac50bd2d4242
Shutarou Takayama <hi.mountain110916@gmail.com>
2022-07-23 13:19:15 +0900

    test

--- go/isuports.go
+++ go/isuports.go
@@ -1118,7 +1118,7 @@ func competitionScoreHandler(c echo.Context) error {
 	}
 	if _, err := tenantDB.NamedExecContext(
 		ctx,
-		"INSERT INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at)",
+		"REPLACE INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at)",
 		playerScoreRows,
 	); err != nil {
 		return fmt.Errorf("error Insert player", err.Error())
